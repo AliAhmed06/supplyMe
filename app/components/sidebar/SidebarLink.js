a2d17dfb9dfea890bdf59989c90618e77dbadca9
DESKTOP-TM8FG9P\Ali Ahmed <alii.amadd@gmail.com>
2023-09-15 18:07:52 +0500

    shop page sidebar change

--- app/components/sidebar/SidebarLink.js
+++ app/components/sidebar/SidebarLink.js
@@ -1,9 +1,14 @@
+import { BiSolidDownArrow } from 'react-icons/bi';
+
 import Link from 'next/link'
 import React from 'react'
 
 const SidebarLink = ({linkText, linkUrl}) => {
   return (
-    <Link href={linkUrl} className=' text-gray2 text-sm font-semibold hover:underline  md:text-lg ' >{linkText}</Link>
+    <div className=' flex items-center gap-3'>
+      <Link href={linkUrl} className=' text-gray2 text-sm font-semibold hover:underline  md:text-lg ' >{linkText}</Link>
+      <BiSolidDownArrow className='text-gray1 h-[10px]' />
+    </div>
   )
 }
 
